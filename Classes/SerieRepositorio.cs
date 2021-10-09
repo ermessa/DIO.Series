6afd6efa4e5669a86a54099f9809791fce1aeb81
Eduardo Messa <87573592+ermessa@users.noreply.github.com>
2021-10-09 17:36:20 -0300

    Adicionar arquivos de projeto.

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using DIO.Series.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> ListaSerie = new List<Serie>();
+        public void Atualiza(int Id, Serie Objeto) 
+        {
+            ListaSerie[Id] = Objeto;
+        }
+
+        public void Exclui(int Id)
+        {
+            ListaSerie[Id].Exclui();
+        }
+
+        public void Insere(Serie Objeto)
+        {
+            ListaSerie.Add(Objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return ListaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return ListaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int Id)
+        {
+            return ListaSerie[Id];
+        }
+    }
+}
---
+++ Interface/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series.Interface
+{
+    public interface IRepositorio <T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int Id);
+        void Insere(T Entidade);
+        void Exclui(int Id);
+        void Atualiza(int Id, T Entidade);
+        int ProximoId();
+    }
+}
